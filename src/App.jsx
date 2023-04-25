dab564b28642af9752b52689e96d7e135c2dcc68
pevescarol <carol.peves@gmail.com>
2023-04-25 15:40:45 -0300

    se agregga rutas para el index y se renderiza el componente grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import './App.css';
 import PeliculasGrid from './components/PeliculasGrid';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <header className='title'>
-        Peliculas
+    <BrowserRouter>
+      <header>
+        <Link to='/'>
+          <h1 className='title'>Peliculas</h1>
+        </Link>
       </header>
-      <main>
-          <PeliculasGrid />
-        </main>
-    </div>
+
+      <Routes>
+        <Route path='/' element={<PeliculasGrid />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
