7fb19d369389d5c86a98a2d34e62934c2afb5e33
pevescarol <carol.peves@gmail.com>
2023-04-25 14:32:13 -0300

    se hace un llamado a la API de TMDB

---
+++ src/utils/httpCliente.js
@@ -0,0 +1,12 @@
+// llamada a la API de TMDB
+const API = 'https://api.themoviedb.org/3'
+
+export const get = (path) => {
+  return fetch (API+path, {
+    headers: {
+      Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDUwN2M3MGI5ODUyYmViMTBmNjdjMzNhYTk5YTM0ZSIsInN1YiI6IjY0MjVmNjk4MDFiMWNhMDA3Nzc5YjNiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aadfO1HT6JAt2bnRbOxvatL2e3PSTQMpsxHGQn1FvsQ',
+      'Content-Type': 'application/json;charset=utf-8'
+    }
+  }).then((results) => results.json())
+}  
