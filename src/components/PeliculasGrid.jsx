5545eb8ef7e1776a14d1937312532695a4f030f8
pevescarol <carol.peves@gmail.com>
2023-04-25 15:13:40 -0300

    useEffect y useState para la llamada a la API

--- src/components/PeliculasGrid.jsx
+++ src/components/PeliculasGrid.jsx
@@ -1,8 +1,18 @@
 import { get } from '../utils/httpCliente'
 import PeliculasCard from './PeliculasCard'
 import '../styles/PeliculasGrid.css'
+import { useState, useEffect } from 'react'
 
 const PeliculasGrid = () => {
+
+  const [peliculas, setPeliculas] = useState([])
+
+  useEffect(() => {
+    get('/discover/movie').then((data) => {
+      setPeliculas(data.results)
+    })
+  }, [])
+
   return(
     <ul className='moviesGrid'>
       {peliculas.map((pelicula) => (
