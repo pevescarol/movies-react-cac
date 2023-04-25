aeca76a27d0c2e2c1b054330c2ea7a9d1456787d
pevescarol <carol.peves@gmail.com>
2023-04-25 13:15:54 -0300

    se agrega imagenes al componente Card

--- src/components/PeliculasCard.jsx
+++ src/components/PeliculasCard.jsx
@@ -1,7 +1,11 @@
 
 const PeliculasCard = ({ pelicula }) => {
+
+  const imgURL = `https://image.tmdb.org/t/p/w300${pelicula.poster_path}`
+
   return (
     <li className='movieCard'>
+      <img className="movieImage" src={imgURL} alt={pelicula.title} />
       <div>{pelicula.title}</div>
     </li>
 )
