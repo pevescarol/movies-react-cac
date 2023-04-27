81257065fb4b2c86f313490a825fe0ac27dfe396
pevescarol <carol.peves@gmail.com>
2023-04-26 23:43:27 -0300

    se agrega Link para las rutas

--- src/components/PeliculasCard.jsx
+++ src/components/PeliculasCard.jsx
@@ -1,14 +1,17 @@
 import '../styles/PeliculasCard.css'
+import { Link } from 'react-router-dom'
 
 const PeliculasCard = ({ pelicula }) => {
 
   const imgURL = `https://image.tmdb.org/t/p/w300${pelicula.poster_path}`
 
   return (
-    <li className='movieCard'>
-      <img className="movieImage" src={imgURL} alt={pelicula.title} />
-      <div>{pelicula.title}</div>
-    </li>
+    <Link to={`/pelicula/${pelicula.id}`}>
+      <li className='movieCard'>
+        <img className="movieImage" src={imgURL} alt={pelicula.title} />
+        <div>{pelicula.title}</div>
+      </li>
+    </Link>
 )
 }
 
