89127bc9e68a517851446a14f3550be4149c6e8e
pevescarol <carol.peves@gmail.com>
2023-05-14 01:03:29 -0300

    se modifica el responsive de detalle de pelicula

--- src/pages/DetallePelicula.jsx
+++ src/pages/DetallePelicula.jsx
@@ -27,16 +27,16 @@ const DetallePelicula = () => {
         <img className='object-cover object-top  w-full h-full' src={`https://image.tmdb.org/t/p/w1280${pelicula.backdrop_path}`} alt={pelicula.title} />
       </div>
 
-      <div className='-mt-[150px] mx-32 flex items-center relative z-10 mobile:block'>
+      <div className='-mt-[150px] md:mx-32 flex items-center justify-center relative z-10 mobile:block'>
         <img className='w-[200px] min-w-[200px] h-[300px] mobile:mx-auto' src={imgURL} alt={pelicula.title} />
-        <div className='px-3 flex flex-col items-start gap-3 mobile:items-center'>
-          <p className=' text-xl'>{pelicula.title}</p>
-          <ul className='flex items-center gap-3 mobile:flex-wrap'>
+        <div className='md:ml-6 px-3 pt-8 flex flex-col items-start gap-3 mobile:items-center'>
+          <p className=' text-xl text-white mobile:text-black font-bold'>{pelicula.title}</p>
+          <ul className='flex items-center justify-center gap-3 mobile:flex-wrap'>
               {pelicula.genres.map((genre) =>(
-                <li key={genre.name} className='px-3 py-1.5 bg-primary rounded-lg text-sm'>{genre.name}</li>
+                <li key={genre.name} className='px-3 py-1.5 bg-indigo-500 rounded-lg text-md font-semibold text-white'>{genre.name}</li>
               ) )}
           </ul>
-          <p className='opacity-[0.9] mobile:text-center'>{pelicula.overview}</p>
+          <p className='opacity-[0.9] mobile:text-center mobile:px-6'>{pelicula.overview}</p>
         </div>
       </div>
     </>
