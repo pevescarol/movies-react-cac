8859016238249caf705cea47e917e84d32035fa9
pevescarol <carol.peves@gmail.com>
2023-05-23 23:19:45 -0300

    se centra el texto del buscador

--- src/components/PeliculasGrid.jsx
+++ src/components/PeliculasGrid.jsx
@@ -55,7 +55,7 @@ const PeliculasGrid = () => {
             <input 
               type="text" 
               onChange={e => setSearchKey(e.target.value)} 
-              className='sm:mb-0 px-5 py-3 bg-indigo-100 text-indigo-700 transform hover:bg-white active:top-0.5 placeholder:text-indigo-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 focus-visible outline-none max-w-[500px] md:w-[500px] p-2 rounded-xl border-none shadow-md shadow-zinc-800/5'
+              className='sm:mb-0 px-5 py-3 bg-indigo-100 text-indigo-700 mobile:text-center transform hover:bg-white active:top-0.5 placeholder:text-indigo-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 focus-visible outline-none max-w-[500px] md:w-[500px] p-2 rounded-xl border-none shadow-md shadow-zinc-800/5'
               placeholder='¿Qué peli estas buscando?'/>
             <button 
                 className='px-5 py-3 bg-indigo-500 shadow rounded-xl text-indigo-100 hover:bg-indigo-600 hover:text-white active:top-0.5'
