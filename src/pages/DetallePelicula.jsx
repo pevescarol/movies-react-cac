473b2a03d86f23ef36899b5a2f761f2bd1f36e81
pevescarol <carol.peves@gmail.com>
2023-05-23 23:27:49 -0300

    se hacen cambios en los margenes para el responsive

--- src/pages/DetallePelicula.jsx
+++ src/pages/DetallePelicula.jsx
@@ -37,7 +37,7 @@ const DetallePelicula = () => {
         <img className='object-cover object-top mt-[100px] rounded-none  w-full h-full' src={`https://image.tmdb.org/t/p/w1280${pelicula.backdrop_path}`} alt={pelicula.title} />
       </div>
 
-      <div className='-mt-[150px] md:mx-32 flex items-center justify-center relative z-10 mobile:block'>
+      <div className='-mt-[150px] md:mx-32 flex items-center justify-center relative z-10 mobile:block mobile:pb-10'>
         <img className='w-[200px] min-w-[200px] h-[300px] mobile:mx-auto' src={imgURL} alt={pelicula.title} />
         <div className='md:ml-6 px-3 pt-8 flex flex-col items-start gap-3 mobile:items-center'>
           <p className=' text-xl text-white mobile:text-black font-bold'>{pelicula.title}</p>
@@ -46,7 +46,7 @@ const DetallePelicula = () => {
                 <li key={genre.name} className='px-3 py-1.5 bg-indigo-500 rounded-lg text-md font-semibold text-white'>{genre.name}</li>
               ) )}
           </ul>
-          <p className='opacity-[0.9] mobile:text-center mobile:px-6'>{pelicula.overview}</p>
+          <p className='opacity-[0.9] mobile:text-center mobile:px-6 mt-10 mobile:m-0'>{pelicula.overview}</p>
         </div>
       </div>
     </>
