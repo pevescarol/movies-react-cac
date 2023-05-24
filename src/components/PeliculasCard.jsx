31f1ff3bceebeed6d74f6a02f0b3e216a6c0e2f4
pevescarol <carol.peves@gmail.com>
2023-05-23 23:08:05 -0300

    se ajusta el elemento li y su estilo

--- src/components/PeliculasCard.jsx
+++ src/components/PeliculasCard.jsx
@@ -5,14 +5,14 @@ const PeliculasCard = ({ pelicula }) => {
   const imgURL = `https://image.tmdb.org/t/p/w300${pelicula.poster_path}`
 
   return (
-    <Link to={`/pelicula/${pelicula.id}`} className='m-0 cursor-pointer overflow-hidden relative isolate group'>
-      <li className='p-0'>
+    <li className='p-0 m-0 cursor-pointer overflow-hidden relative isolate group'>
+      <Link to={`/pelicula/${pelicula.id}`} >
         <img className='rounded-1 ' src={imgURL} alt={pelicula.title} />
         <div className='w-[80%] absolute mx-auto p-2 bottom-0 md:bottom-[-10rem] left-[50%] translate-x-[-50%] text-center text-white backdrop-blur-sm bg-gray-900/[0.2] rounded-lg z-10 ease-in duration-300 group-hover:bottom-3 '>
           <p className='text-center text-lg'>{pelicula.title}</p>
         </div>
-      </li>
-    </Link>
+      </Link>
+    </li>
 )
 }
 
