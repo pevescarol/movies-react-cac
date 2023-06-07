c55a5fcee76ddb38e4d2d1d96c90c89ae97faea9
pevescarol <carol.peves@gmail.com>
2023-06-07 16:38:37 -0300

    se crea el componente Buscador

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,9 +1,11 @@
+import Buscador from "../components/Buscador"
 import PeliculasGrid from "../components/PeliculasGrid"
 
 
 const LandingPage = () => {
   return (
     <main className="max-w-[1200px] mx-auto px-5">
+      <Buscador />
       <PeliculasGrid />
     </main>
   )
